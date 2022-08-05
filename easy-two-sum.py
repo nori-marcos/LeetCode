2f2b666c7f3360803a8073a0f542a81e32ad66fe
Marcos Noriyuki Miyata <84037445+nori-marcos@users.noreply.github.com>
2022-08-05 16:05:23 -0300

    brute force, two sum implementation

--- easy-two-sum.py
+++ easy-two-sum.py
@@ -1,1 +1,12 @@
+nums = [3, 3]
+target = 6
 
+indexes = []
+      
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        if nums[i] + nums [j] == target:
+            indexes.append(i)
+            indexes.append(j)
+            
+print(indexes)
