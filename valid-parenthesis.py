cbd750860ff6a5ea395f8f02ebfb902a55181814
Marcos Noriyuki Miyata <84037445+nori-marcos@users.noreply.github.com>
2022-08-27 14:19:22 -0300

    valid parenthesis easy challenge.

---
+++ valid-parenthesis.py
@@ -0,0 +1,34 @@
+def is_match(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "[" and p2 == "]":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    else:
+        return False
+        
+stack = []
+is_balanced = True
+
+str = "(]"
+
+chars = str.split("")
+        
+for i, char in enumerate(chars):
+    if char  in "{[(":
+        stack.append(char)
+    else:
+        if len(stack) == 0:
+            is_balanced = False
+            break
+        else:
+            top = stack.pop()
+            if not is_match(top, char):
+                is_balanced = False
+                break
+        
+if len(stack) == 0 and is_balanced:
+    print("true")
+else:
+    print("false")
